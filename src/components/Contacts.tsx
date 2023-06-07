67845424a1993ae4045b6c42b886f393c95ec5fc
dimakuznetsow <108948435+dimakuznetsow@users.noreply.github.com>
2023-06-07 21:07:12 +0300

    styling

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -11,17 +11,17 @@ function Contacts() {
             <div className="mx-80 pb-80">
                 <p className="text-2xl font-bold mt-4">Get in Touch. Connect, Collaborate, Code.</p>
                 <ul className="flex justify-start mt-20 gap-16 items-center">
-                    <li className="bg-base-200  p-4 rounded-full shadow-sm">
+                    <li className="bg-base-200  p-4 rounded-full">
                         <a className="flex items-center gap-2" href="https://github.com/dimakuznetsow" target="_blank">
                             <ImGithub size={44} />
                         </a>
                     </li>
-                    <li className="bg-base-200 p-4 rounded-full shadow-sm">
+                    <li className="bg-base-200 p-4 rounded-full">
                         <a className="flex items-center gap-2" href="https://www.linkedin.com/in/dimakuznetsow/" target="_blank">
                             <ImLinkedin size={44} />
                         </a>
                     </li>
-                    <li className="bg-base-200 p-4 rounded-full shadow-sm">
+                    <li className="bg-base-200 p-4 rounded-full">
                         <a className="flex items-center gap-2" href="mailto:dimakuznetsow@gmail.com" >
                             <FiMail size={44} />
                         </a>
