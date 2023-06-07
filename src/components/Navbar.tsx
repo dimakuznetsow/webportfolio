5a8f268591cc2935f41aef568a25899766883f0e
dimakuznetsow <108948435+dimakuznetsow@users.noreply.github.com>
2023-06-07 21:20:54 +0300

    styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link, animateScroll as scroll } from "react-scroll";
 
 function Navbar() {
     return (
-        <div className="navbar bg-base-100 fixed top-0 shadow-sm">
+        <div className="navbar bg-base-100 fixed top-0 shadow z-10">
             <div className="flex-1">
                 <a onClick={scroll.scrollToTop} className="btn no-animation bg-transparent hover:bg-transparent border-none normal-case text-xl text-base-content">Dmitry Kuznetsov</a>
             </div>
